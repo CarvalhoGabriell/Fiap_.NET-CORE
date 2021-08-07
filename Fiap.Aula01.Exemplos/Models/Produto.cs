3fc7dce769ba715d7f08ed74423320c3fe170b3f
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-08-07 14:59:31 -0300

    Exmplos de como fazer sbrescrita e sobrecarga de metodo em C#

--- Fiap.Aula01.Exemplos/Models/Produto.cs
+++ Fiap.Aula01.Exemplos/Models/Produto.cs
@@ -3,17 +3,58 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Fiap.Aula01.Exemplos.Models
-{
-    class Produto
+{   // Uma classe abstrata -> não poode ser instanciada e PODE conter metodos com implementação ou sem
+    abstract class Produto
     {
         /*
          * Atalho para gerar propriedades -> escrever "prop" + tab + tab
+         * Atlaho gerar construtor -> escrever "ctor" + tab + tab
          */
 
         // Propriedades (Getters e Setters)
         public int Id { get; set; }
         public string Nome { get; set; }
         public decimal Preco { get; set; }
+
+        // Contrutores
+        public Produto(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+
+        }
+
+        // Metodos da classe produto
+        public void Cadastrar()
+        {
+            Console.WriteLine($"{Nome} Cadastrado com sucesso!");
+        }
+
+        // Sobrecargas de metodos -> metodo da mesma classe com mesmo nome e com parametros de tipos diferentes ou quantidades de parametros ou a ordem
+        public decimal CalcularDesconto(decimal porcentagem)
+        {
+            return Preco * (100 - porcentagem) /100;
+        }
+
+        public virtual decimal CalcularDesconto(string cupom)
+        {
+            // O cupom FIAP30 dá 30% de desconto
+            if (cupom == "FIAP30")
+            {
+                return Preco * 0.70m;
+
+            }
+            return Preco;
+        }
+        // O modificador "Virtual" Habilita que esse metodo possa ser sobrescrito pelas classes filhas da classe pai
+        public virtual decimal CalcularDesconto()
+        {
+            return Preco * 0.95m; // "m" no final de um numero para definir ele como decimal
+        }
+
+        // Metodo abstrato -> Sem implementtação, termina com ;
+        // Metodo aumenta a inflação em 5%, 10%, 20%....
+        public abstract void AumentarPreco(decimal inflacao);
     }
 }
 
---
+++ Fiap.Aula01.Exemplos/Models/Tenis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fiap.Aula01.Exemplos.Models
+{
+    class Tenis : Produto
+    {
+        //Propriedades. Getter e Setter devem sewr em UpperCase
+        public int Tamanho { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Cor { get; set; }
+
+        // Construtor
+        // : base -> chama o construtor da classe pai. Passa como parametros os parametros do contrutor da classe pai
+        public Tenis(int id, string nome, int tamanho) : base(id, nome)
+        {
+            Tamanho = tamanho;
+        }
+
+        // Metodos 
+        // Sobrescrita -> implementa o metodo do pai na classe filha, para assim alterar/adicionar a implementação
+        public override decimal CalcularDesconto()
+        {
+            // Desconto de 10% para os tenis diferente do padrão que é 5%
+            return Preco * 0.9m;
+        }
+
+        // Sobrescrever o metodo ded CalcularDesconto(cupom) que da 30%, adicionar o cupom FIAP50 para um desconto de 50%
+        public override decimal CalcularDesconto(string cupom)
+        {
+            if (cupom == "FIAP50")
+            {
+                return Preco * 0.5m;
+            }
+            return Preco; 
+        }
+
+        public override void AumentarPreco(decimal inflacao)
+        {
+            Preco += Preco * inflacao / 100;
+        }
+    }
+}
