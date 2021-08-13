4c8ede06e31984bbfb88340fce74afb8ec23bc4a
Gabriel Carvalho <gabriel.fernandes@atomicsolutions.com.br>
2021-08-12 23:02:53 -0300

    Tarefas para terminar

---
+++ Fiap.Aula01.Exemplo2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Fiap.Aula01.Exemplo2.Models;
+
+namespace Fiap.Aula01.Exemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciar uma Loja
+            Loja loja = new Loja()
+            {
+                Porte = PorteLoja.Medio
+            };
+
+            if (loja.Porte == PorteLoja.Medio )
+            {
+                Console.WriteLine("A Loja possui Porte Medio");
+            } 
+            else
+            {
+                Console.WriteLine("A loja nao possui Porte Medio");
+            }
+
+            Console.WriteLine($"Porte da Loja {loja.Porte}");
+
+            // TAREFA
+            //APRESENTAR UM MENU 1-cadastrar, 2-lisrar, 0- sair
+        }
+    }
+}
